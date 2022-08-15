8bf65e1b60302ec8886cd305f2b1e4af4660bcd6
DennisGoldfarb <goldfarb.dennis@wustl.edu>
2022-08-15 10:56:23 -0500

    Isolation shape documentation

--- src/OpenAcquisition/IsolationWindows/isolation_shape.py
+++ src/OpenAcquisition/IsolationWindows/isolation_shape.py
@@ -3,13 +3,13 @@ import math
 class IsolationShape:
     
     def __init__(self, a, b, c, d):
-        """_summary_
+        """Generalized bell-shaped function representing the isolation efficiency shape of quadrupole or linear ion trap. 
 
         Args:
-            a (float): _description_
-            b (float): _description_
-            c (float): _description_
-            d (float): _description_
+            a (float): half-width at half-maximum
+            b (float): proportional to the slope at the edges of the function
+            c (float): offset from center
+            d (float): full maximum
         """
         self.a = a
         self.b = b
@@ -19,34 +19,37 @@ class IsolationShape:
         self.offset_range = self.get_offset_range()
     
     def get_isolation_efficiency(self, offset):
-        """_summary_
+        """Computes the isolation efficiency at a given offset from the center of the isolation window.
 
         Args:
-            offset (float): _description_
+            offset (float): offset from the isolation window's center
 
         Returns:
-            float: _description_
+            float: isolation efficiency
         """
         return self.d / (1 + math.pow(abs((offset - self.c) / self.a), self.b2))
     
-    def get_isolation_efficienct_unshifted(self, offset):
-        """_summary_
+    def get_isolation_efficiency_unshifted(self, offset):
+        """Computes the isolation efficiency at a given offset from the center of the isolation efficiency model.
+        This ignores the c parameter of the generalized bell-shaped function.
 
         Args:
-            offset (float): _description_
+            offset (float): offset from the isolation window's center of isolation efficiency
 
         Returns:
-            float: _description_
+            float: isolation efficiency
         """
         return self.d / (1 + math.pow(abs((offset) / self.a), self.b2))
     
     def get_offset_range(self, efficiency_threshold = 0.01):
-        """_summary_
+        """Returns the positive unshifted offset (ignores the c parameter) with the given isolation efficiency threshold.
+        Since the generalized bell-shaped function is convex and symmetric, the range from the negative and postive
+        values will contain all offsets with efficiency >= efficiency_threshold.
 
         Args:
-            efficiency_threshold (float, optional): _description_. Defaults to 0.01.
+            efficiency_threshold (float, optional): minimum isolation efficiency. Defaults to 0.01.
 
         Returns:
-            float: _description_
+            float: positive unshifted offset with the given isolation efficiency
         """
         return self.a * math.pow((self.d / efficiency_threshold - 1), 1 / self.b2)
