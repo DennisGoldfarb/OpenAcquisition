5470b7566349912d96d03942e37d35749f778a93
Dennis Goldfarb <Goldfarb.dennis@gmail.com>
2022-08-16 20:49:45 -0500

    Development (#6)
    * Documentation (#1)
    
    * start of readme
    
    * isolation shape document in numpy style
    
    * IsolationShape examples
    
    * added imports to examples
    
    * ds_store to gitingnore
    
    * Fixed docstring
    
    * Jupyter notebook tutorial
    
    Co-authored-by: DennisGoldfarb <goldfarb.dennis@wustl.edu>
    
    * upgraded mistune to 2.0.4 (#2)
    
    Co-authored-by: DennisGoldfarb <goldfarb.dennis@wustl.edu>
    
    * Fix/read the docs (#3)
    
    * config
    
    * trying alternative
    
    * cleaning up
    
    Co-authored-by: DennisGoldfarb <goldfarb.dennis@wustl.edu>
    
    * Refactor/organize for ci cd (#4)
    
    * reorganize project
    
    * fixing docs path
    
    * Update ci-cd.yml
    
    Co-authored-by: DennisGoldfarb <goldfarb.dennis@wustl.edu>
    
    * build: added python-semantic-release (#5)
    
    * build: added python-semantic-release
    
    * build: fixed semantic release
    
    Co-authored-by: Dennis Goldfarb <d.goldfarb@wustl.edu>
    
    * 0.0.1
    
    Automatically generated by python-semantic-release
    
    * build: only deploy main branch
    
    Co-authored-by: DennisGoldfarb <goldfarb.dennis@wustl.edu>
    Co-authored-by: Dennis Goldfarb <d.goldfarb@wustl.edu>
    Co-authored-by: semantic-release <semantic-release>

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# OpenAcquisition
+# openacquisition
+
+Python library to create data acquisition algorithms for mass spectrometers
+
+## Requirements
+
+## Installation
+
+```bash
+$ pip install openacquisition
+```
+
+## Usage
+
+## Contributing
+Interested in contributing? Check out the contributing guidelines. 
+Please note that this project is released with a Code of Conduct. 
+By contributing to this project, you agree to abide by its terms.
+
+## License
+`OpenAcquisition` was created by the [Goldfarb Lab](https://sites.wustl.edu/goldfarblab) at Washington University School of Medicine in St. Louis. It is licensed under the terms of the MIT license.
---
+++ openacquisition/isolation_windows/isolation_shape.py
@@ -0,0 +1,137 @@
+import math
+from lazy import lazy
+
+class IsolationShape:
+    """Isolation efficiency model constructor
+        
+        Generalized bell-shaped function representing the isolation efficiency shape of a quadrupole or linear ion trap.
+
+        Parameters
+        ----------
+        a : float
+            half-width at half-maximum
+        b : float
+            proportional to the slope at the edges of the function
+        c : float
+            offset from center
+        d : float
+            full maximum
+            
+        Notes
+        -----
+        Let :math:`y = \\frac{d}{1+\\left|\\frac{x-c}{a}\\right|^{2b}}`, where :math:`y` represents the isolation efficiency,
+        and :math:`x` represents the offset from the center of the window.
+        
+        Examples
+        --------
+        >>> from openacquisition.isolation_windows.isolation_shape import IsolationShape
+        >>> shape = IsolationShape(1, 10, 0, 1)
+        """
+    
+    def __init__(self, a, b, c, d):
+        """Constructor
+        """
+        self.a = a
+        self.b = b
+        self.b2 = 2*b
+        self.c = c
+        self.d = d
+    
+    def get_isolation_efficiency(self, offset):
+        """Computes the isolation efficiency at a given offset from the center of the isolation window.
+
+        Parameters
+        ----------
+        offset : float
+            offset from the isolation window's center
+
+        Returns
+        -------
+        float
+            isolation efficiency
+        
+        Examples
+        --------
+        >>> from openacquisition.isolation_windows.isolation_shape import IsolationShape
+        >>> shape = IsolationShape(1, 10, 0, 1)
+        >>> shape.get_isolation_efficiency(0.0)
+        """
+        
+        return self.d / (1 + math.pow(abs((offset - self.c) / self.a), self.b2))
+    
+    def get_isolation_efficiency_unshifted(self, offset):
+        """Computes the isolation efficiency at a given offset from the center of the isolation efficiency model.
+
+        Parameters
+        ----------
+        offset : float
+            offset from the isolation window's center of isolation efficiency
+
+        Returns
+        -------
+        float
+            isolation efficiency
+            
+        Notes
+        -----
+        This ignores the c parameter of the generalized bell-shaped function.
+        
+        Examples
+        --------
+        >>> from openacquisition.isolation_windows.isolation_shape import IsolationShape
+        >>> shape = IsolationShape(1, 10, 1, 1)
+        >>> shape.get_isolation_efficiency_unshifted(0.0)
+        """
+        return self.d / (1 + math.pow(abs((offset) / self.a), self.b2))
+    
+   
+    def get_offset_range(self, efficiency_threshold = 0.01):
+        """Returns the positive unshifted offset (ignores the c parameter) with the given isolation efficiency threshold
+        
+        Parameters
+        ----------
+        efficiency_threshold : float, optional
+            minimum isolation efficiency, by default 0.01
+
+        Returns
+        -------
+        float
+            positive unshifted offset with the given isolation efficiency
+            
+        Notes
+        -----
+        Since the generalized bell-shaped function is convex and symmetric, the range from the negative and postive
+        values will contain all offsets with efficiency >= efficiency_threshold.
+        
+        Examples
+        --------
+        >>> from openacquisition.isolation_windows.isolation_shape import IsolationShape
+        >>> shape = IsolationShape(1, 10, 0, 1)
+        >>> shape.get_offset_range()
+        >>> shape.get_offset_range(0.01)
+        >>> shape.get_offset_range(0.001)
+        """
+        return self.a * math.pow((self.d / efficiency_threshold - 1), 1 / self.b2)
+    
+    
+    @lazy
+    def offset_range(self):
+        """Returns the positive unshifted offset (ignores the c parameter) at 0.01 isolation efficiency
+        
+        Returns
+        -------
+        float
+            positive unshifted offset at 0.01 isolation efficiency
+            
+        Notes
+        -----
+        Lazy evaluation. Will only be computed at first request and then the result will be cached.
+        
+        Examples
+        --------
+        >>> from openacquisition.isolation_windows.isolation_shape import IsolationShape
+        >>> shape = IsolationShape(1, 10, 0, 1)
+        >>> shape.offset_range()
+        """
+        return self.get_offset_range()
+    
