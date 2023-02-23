e13c96e535aa196b5c951c8758365ce5a2023e47
DennisGoldfarb <goldfarb.dennis@wustl.edu>
2023-02-23 09:36:06 -0600

    feat(isotopes) isotope splines and distributions

--- openacquisition/mathematics/splines.py
+++ openacquisition/mathematics/splines.py
@@ -1,12 +1,14 @@
-from bisect import bisect_left
+from bisect import bisect_right
 
 class CubicSpline:
     def __init__(self, a, b, c, d, knots):     
         # Validate input length
-        it = iter([a,b,c,d,knots])
+        it = iter([a,b,c,d])
         the_len = len(next(it))
         if not all(len(l) == the_len for l in it):
-            raise ValueError('CubicSpline init: Not all input lists have same length.')
+            raise ValueError('CubicSpline init: Not all input coefficient lists have the same length.')
+        if not len(knots) == len(a) + 1:
+            raise ValueError('CubicSpline init: knots length should be 1 more than the coefficient lengths.')
         
         self.a = a # constant spline coefficients
         self.b = b # linear spline coefficients
@@ -14,11 +16,9 @@ class CubicSpline:
         self.d = d # cubic spline coefficients
         self.knots = knots # knots
      
+    # For speed purposes we're not checking if x is in bounds. Assume the programmer has checked this beforehand.
     def eval(self, x):
-        index = bisect_left(self.knots, x) - 1
-        
-        if index < 0:
-            index = 0
+        index = bisect_right(self.knots, x) - 1
         
         xx = x - self.knots[index]
 
--- tests/mathematics/test_splines.py
+++ tests/mathematics/test_splines.py
@@ -2,19 +2,19 @@ import pytest
 from openacquisition.mathematics.splines import CubicSpline
 
 def test_init():
-    spline = CubicSpline([1,1], [2,2], [3,3], [4,4], [50, 100])
+    spline = CubicSpline([1,1], [2,2], [3,3], [4,4], [50, 100, 150])
     assert spline.a == [1,1]
     assert spline.b == [2,2]
     assert spline.c == [3,3]
     assert spline.d == [4,4]
-    assert spline.knots == [50,100]
+    assert spline.knots == [50,100,150]
     
     # Inputs of varying sizes should raise an error
     with pytest.raises(ValueError) as exc_info:
-        spline = CubicSpline([1,1,1], [2,2], [3,3], [4,4], [50, 100])
+        spline = CubicSpline([1,1,1], [2,2], [3,3], [4,4], [50, 100, 150])
     
 def text_inBounds():
-    spline = CubicSpline([1,1], [2,2], [3,3], [4,4], [50, 100])
+    spline = CubicSpline([1,1], [2,2], [3,3], [4,4], [50, 100, 150])
     assert spline.inBounds(50) == True
     assert spline.inBounds(49) == False
     assert spline.inBounds(100) == True
@@ -22,8 +22,7 @@ def text_inBounds():
     assert spline.inBounds(75) == True
     
 def test_eval():
-    spline = CubicSpline([1,1], [2,2], [3,3], [4,4], [50, 100])
+    spline = CubicSpline([1,1], [2,2], [3,3], [4,4], [50, 100, 150])
     assert spline.eval(50) == 1
     assert spline.eval(51) == 10
-    assert spline.eval(49) == -2
     
